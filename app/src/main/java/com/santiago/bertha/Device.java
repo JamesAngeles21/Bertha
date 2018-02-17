94708b05217dedb4c86d5506ae720acfabc07476
Jason Dong <Jason.Dong@StonyBrook.edu>
2018-02-17 13:55:57 -0500

    Created Plant, Device, and enums for fields in Plants

---
+++ app/src/main/java/com/santiago/bertha/Device.java
@@ -0,0 +1,14 @@
+package com.santiago.bertha;
+
+import java.util.List;
+
+/**
+ * Created by Jason on 2/17/2018.
+ */
+
+public class Device {
+
+    String name;
+    List<Plant> plants;
+
+}
---
+++ app/src/main/java/com/santiago/bertha/State.java
@@ -0,0 +1,14 @@
+package com.santiago.bertha;
+
+/**
+ * Created by Jason on 2/17/2018.
+ */
+
+public enum State {
+    COLD,
+    HOT,
+    BRIGHT,
+    DARK,
+    DRY,
+    WET,
+}
