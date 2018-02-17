18adc6679d3269d2c0c3bd4e3787b68319ac6d77
Agnieszka Gawrys <agnieszka.gawrys@stonybrook.edu>
2018-02-17 15:40:02 -0500

    Re-pushing deleted code

--- app/src/main/java/com/santiago/bertha/devicetab.java
+++ app/src/main/java/com/santiago/bertha/devicetab.java
@@ -1,8 +1,37 @@
 package com.santiago.bertha;
 
+import android.app.Fragment;
+import android.view.View;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 /**
  * Created by Agnieszka on 2/17/2018.
  */
 
-public class devicetab {
+public class devicetab extends android.support.v4.app.Fragment {
+
+    private Button btnTEST;
+    private static final String TAG = "devicetab";
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.devicestab, container, false);
+
+        btnTEST = (Button) view.findViewById(R.id.btnTEST2);
+
+        btnTEST.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Testing button 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
 }
--- app/src/main/java/com/santiago/bertha/planttab.java
+++ app/src/main/java/com/santiago/bertha/planttab.java
@@ -1,8 +1,35 @@
 package com.santiago.bertha;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 /**
  * Created by Agnieszka on 2/17/2018.
  */
 
-public class planttab {
+public class planttab extends Fragment {
+
+    private Button btnTEST;
+    private static final String TAG = "planttab";
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.plantstab, container, false);
+
+            btnTEST = (Button) view.findViewById(R.id.btnTEST);
+
+        btnTEST.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Testing button 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+            return view;
+    }
 }
